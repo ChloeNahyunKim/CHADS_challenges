7f0be72e9145745d2dfb0e07aa07ba26c6a1e67e
Chloe Kim <chloekim@Chloes-MacBook-Air.local>
2024-02-20 18:43:40 -0700

    add binaryToAscii

---
+++ binaryToAscii/convert.py
@@ -0,0 +1,22 @@
+def main():
+    answerStatus = False
+    print("hey there soon-to-be chad. Are you ready to Solve the \
+legendary awesome sauCe challenges ahead of you? \nWell.. you need to fIrst solve thIs challenge. \n\
+        ┌─────────────────────────────────────────────────────────────────────────────────────────────────────────┐\n\
+            01100011 01101111 01100100 01100101 00111010 01000010 01001001 01001110 01000111 01010101 01010011 \n\
+        └─────────────────────────────────────────────────────────────────────────────────────────────────────────┘\n\
+        　　ᕱ ᕱ ||\n\
+        　 ( ･ω･||\n\
+        　 /　つΦ\n\
+        ")
+    answer = input("What's the secret code? : ")
+    while answerStatus == False:
+        if answer == 'BINGUS':
+            answerStatus = True
+            break
+        answer = input("No no.. that's not the right answer. What's the secret code? : ")
+
+    print('Good work!!! Here is the next challenge: insert nc here')
+
+if __name__ == "__main__":
+    main()
